58122802d179415565ee8b76f98264f8e557a6d6
devbn3li <fudwhc@gmail.com>
2024-08-19 18:59:06 +0300

    Enhance navbar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,17 +3,20 @@ import Link from "next/link";
 
 const Nav = () => {
   return (
-    <nav className="text-white text-2xl p-5 flex justify-evenly">
-      <h1 className="">Mohamed Ali</h1>
-      <ul className="gap-4 flex ">
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
+    <nav className="bg-black text-white text-lg md:text-2xl py-5 px-10 md:px-20 flex justify-between items-center shadow-lg">
+      <h1 className="font-bold text-xl md:text-3xl">Mohamed Ali</h1>
+      <ul className="flex gap-6 md:gap-10">
+        <li className="relative group">
           <Link href="#about">About</Link>
+          <span className="block h-0.5 bg-[rgb(100_108_255_/_var(--tw-text-opacity))] absolute left-0 bottom-0 w-0 group-hover:w-full transition-all duration-300 ease-in-out"></span>
         </li>
-        <li>
+        <li className="relative group">
+          <Link href="#work">Work</Link>
+          <span className="block h-0.5 bg-[rgb(100_108_255_/_var(--tw-text-opacity))] absolute left-0 bottom-0 w-0 group-hover:w-full transition-all duration-300 ease-in-out"></span>
+        </li>
+        <li className="relative group">
           <Link href="#contact">Contact</Link>
+          <span className="block h-0.5 bg-[rgb(100_108_255_/_var(--tw-text-opacity))] absolute left-0 bottom-0 w-0 group-hover:w-full transition-all duration-300 ease-in-out"></span>
         </li>
       </ul>
     </nav>
