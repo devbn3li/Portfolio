f4809648ef92058b9a4133fd135d60a2cc292d3e
devbn3li <fudwhc@gmail.com>
2024-08-19 18:59:40 +0300

    Enhance Nav component

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,18 +4,35 @@ import Link from "next/link";
 const Nav = () => {
   return (
     <nav className="bg-black text-white text-lg md:text-2xl py-5 px-10 md:px-20 flex justify-between items-center shadow-lg">
-      <h1 className="font-bold text-xl md:text-3xl">Mohamed Ali</h1>
+      <h1 className="font-bold text-xl md:text-3xl text-opacity-70 hover:text-opacity-100 transition-all duration-300">
+        Mohamed Ali
+      </h1>
       <ul className="flex gap-6 md:gap-10">
         <li className="relative group">
-          <Link href="#about">About</Link>
+          <Link
+            href="#about"
+            className="text-white text-opacity-70 hover:text-opacity-100 transition-all duration-300"
+          >
+            About
+          </Link>
           <span className="block h-0.5 bg-[rgb(100_108_255_/_var(--tw-text-opacity))] absolute left-0 bottom-0 w-0 group-hover:w-full transition-all duration-300 ease-in-out"></span>
         </li>
         <li className="relative group">
-          <Link href="#work">Work</Link>
+          <Link
+            href="#work"
+            className="text-white text-opacity-70 hover:text-opacity-100 transition-all duration-300"
+          >
+            Work
+          </Link>
           <span className="block h-0.5 bg-[rgb(100_108_255_/_var(--tw-text-opacity))] absolute left-0 bottom-0 w-0 group-hover:w-full transition-all duration-300 ease-in-out"></span>
         </li>
         <li className="relative group">
-          <Link href="#contact">Contact</Link>
+          <Link
+            href="#contact"
+            className="text-white text-opacity-70 hover:text-opacity-100 transition-all duration-300"
+          >
+            Contact
+          </Link>
           <span className="block h-0.5 bg-[rgb(100_108_255_/_var(--tw-text-opacity))] absolute left-0 bottom-0 w-0 group-hover:w-full transition-all duration-300 ease-in-out"></span>
         </li>
       </ul>
